98cb98ce3d9677db6916d3c0261385f6d17fae7a
Rohit Jose <rohitjose@gmail.com>
2019-02-17 15:24:52 +1100

    Updates test cases for company endpoint

--- app/company/tests/test_company_api.py
+++ app/company/tests/test_company_api.py
@@ -1,14 +1,29 @@
 import json
 from graphene.test import Client
+
 from app.schema import schema
-from django.test import TestCase
+from core.models import Person, Company
+from core.tests.test_models import MongoTestCase
 
 
-class CompanyApiTests(TestCase):
+class CompanyApiTests(MongoTestCase):
     """Tests the APIs for the Company entity"""
 
     def setUp(self):
+        super().setUp()
         self.client = Client(schema)
+        # Populate the test collectiom with person data
+        with open("core/tests/test_data/employees.json") as employee_json:
+            employees = json.load(employee_json)
+            for employee in employees:
+                employee_obj = Person(**employee)
+                employee_obj.save()
+        # Populate the test collection with company data
+        with open("core/tests/test_data/companies.json") as companies_json:
+            companies = json.load(companies_json)
+            for companies in companies:
+                companies_obj = Company(**companies)
+                companies_obj.save()
 
     def test_employee_retrieval_no_employees(self):
         """Tests if the API returns an error when there are no employees"""
